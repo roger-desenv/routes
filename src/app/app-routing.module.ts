a335fd8499fcf7f7e5c277d3f0d7fffb038cffb2
Roger Costa <roger.desenv@gmail.com>
2023-05-17 07:41:44 -0300

    adicionando path nas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
+
+const routes: Routes = [
+  {path:'', component: TitleComponent, pathMatch: 'full'},
+  {path:'portfolio', component: CardComponent, pathMatch:'prefix'},
+  {path:'**', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/index/title/title.component.ts
+++ src/app/pages/index/title/title.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
